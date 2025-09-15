99aac7d5a839578f10d74df4ab0d7303b06078af
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-09-14 23:08:34 -0300

    fix: ajusta tratamento de remain.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 15:42:11 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/09/14 21:48:09 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/09/14 23:05:47 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,38 +52,37 @@ char	*set_remain(char *remain)
 	nl_index = get_nl_char(remain);
 	tmp = NULL;
 	tmp = ft_calloc(r_size - nl_index, sizeof(char));
-	ft_strlcpy(tmp, (remain + (int)nl_index), r_size - nl_index);
+	ft_strlcpy(tmp, (remain + (int)nl_index) + 1, r_size - nl_index);
 	free(remain);
 	remain = tmp;
 	return (remain);
 }
 
-char	*set_line(char *remain, char *buffer)
+char	*set_line(char **remain, char *buffer)
 {
 	char	*return_ptr;
 	char	*tmp;
 
 	return_ptr = NULL;
 	tmp = NULL;
-	if (!(get_nl_char(remain) < 0))   // if has no '\n' join all
+	if (!(get_nl_char(*remain) < 0))  // has \n
 	{
-		return_ptr = ft_calloc(get_nl_char(remain) + 2, sizeof(char));
-		ft_strlcpy(return_ptr, remain, get_nl_char(remain) + 2);
-		remain = set_remain(remain);
+		return_ptr = ft_calloc(get_nl_char(*remain) + 2, sizeof(char));
+		ft_strlcpy(return_ptr, *remain, get_nl_char(*remain) + 2);
+		*remain = set_remain(*remain);
 		return (return_ptr);
 	}
 	else if (get_nl_char(buffer) >= 0)
 		{
 			tmp = ft_calloc(get_nl_char(buffer) + 2, sizeof(char));
 			ft_strlcpy(tmp, buffer, get_nl_char(buffer) + 2);
-			return_ptr = ft_strjoin(remain, tmp);
+			return_ptr = ft_strjoin(*remain, tmp);
 			free(tmp);
-			free(remain);
-			remain = ft_strjoin(buffer + get_nl_char(buffer) + 1, "");
-			
+			free(*remain);
+			*remain = ft_strjoin(buffer + get_nl_char(buffer) + 1, "");
 		}
 	else if (ft_strlen(buffer))
-		return_ptr = ft_strjoin(remain, buffer);
+		return_ptr = ft_strjoin(*remain, buffer);
 	return (return_ptr);
 }
 
@@ -94,19 +93,12 @@ char	*get_next_line(int fd)
 	char		*line;
 	static char	*remain;
 
-	// fill buffer (deal with reading of file)
 	buffer = ft_calloc(1, 1);
-	// line = ft_calloc(1, 1);
-
-	// check_remain(*remain);
 	if (remain == NULL)
 		remain = ft_calloc(1, 1);
-		
 	if (get_nl_char(remain) < 0)
 		fill_buffer(fd, &buffer);
-	// join with remain and buffer into line
-	line = set_line(remain, buffer);
-	// set_remain(&remain);
+	line = set_line(&remain, buffer);
 	free(buffer);
 	return (line);
 }
